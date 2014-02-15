a9290fb7676a9f798fbd9783cb965298ba947b33
Chunwei Chen <tuxoko@gmail.com>
2014-02-15 23:21:49 +0800

    Modify/Add incremental checksum function for abd_iterate_rfunc
    Modify/Add incremental fletcher and sha256 function prototype to match
    abd_iterate_rfunc callback type. Also, reduce duplicated code a bit in
    zfs_fletcher.c.
    
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- include/sys/zio_checksum.h
+++ include/sys/zio_checksum.h
@@ -61,6 +61,20 @@ extern zio_checksum_info_t zio_checksum_table[ZIO_CHECKSUM_FUNCTIONS];
 /*
  * Checksum routines.
  */
+static inline void
+zio_checksum_SHA256_init(zio_cksum_t *zcp)
+{
+	uint32_t H[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+	    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
+
+	ZIO_SET_CHECKSUM(zcp,
+	    (uint64_t)H[0] << 32 | H[1],
+	    (uint64_t)H[2] << 32 | H[3],
+	    (uint64_t)H[4] << 32 | H[5],
+	    (uint64_t)H[6] << 32 | H[7]);
+}
+
+extern int zio_checksum_SHA256_incremental(const void *, uint64_t, void *);
 extern zio_checksum_t zio_checksum_SHA256;
 
 extern void zio_checksum_compute(zio_t *zio, enum zio_checksum checksum,
