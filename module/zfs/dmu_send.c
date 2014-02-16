2b3aad834bbfac761809bf49f1320b7bcb456ac2
Chunwei Chen <tuxoko@gmail.com>
2014-02-16 19:47:48 +0800

    Handle abd_t in dbuf.c, ddt.c, dmu*.c
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/dmu_send.c
+++ module/zfs/dmu_send.c
@@ -370,6 +370,16 @@ dump_dnode(dmu_sendarg_t *dsp, uint64_t object, dnode_phys_t *dnp)
 	(((uint64_t)dnp->dn_datablkszsec) << (SPA_MINBLOCKSHIFT + \
 	(level) * (dnp->dn_indblkshift - SPA_BLKPTRSHIFT)))
 
+static int
+fillbadblock(void *p, uint64_t size, void *private)
+{
+	int i;
+	uint64_t *x = p;
+	for (i = 0; i < size >> 3; i++)
+		x[i] = 0x2f5baddb10cULL;
+	return 0;
+}
+
 /* ARGSUSED */
 static int
 backup_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
@@ -406,7 +416,7 @@ backup_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
 		    &aflags, zb) != 0)
 			return (SET_ERROR(EIO));
 
-		blk = abuf->b_data;
+		blk = ABD_TO_LINEAR(abuf->b_data);
 		for (i = 0; i < blksz >> DNODE_SHIFT; i++) {
 			uint64_t dnobj = (zb->zb_blkid <<
 			    (DNODE_BLOCK_SHIFT - DNODE_SHIFT)) + i;
@@ -425,32 +435,32 @@ backup_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
 		    &aflags, zb) != 0)
 			return (SET_ERROR(EIO));
 
-		err = dump_spill(dsp, zb->zb_object, blksz, abuf->b_data);
+		err = dump_spill(dsp, zb->zb_object, blksz, ABD_TO_LINEAR(abuf->b_data));
 		(void) arc_buf_remove_ref(abuf, &abuf);
 	} else { /* it's a level-0 block of a regular object */
 		uint32_t aflags = ARC_WAIT;
 		arc_buf_t *abuf;
 		int blksz = BP_GET_LSIZE(bp);
+		void *buf;
 
 		if (arc_read(NULL, spa, bp, arc_getbuf_func, &abuf,
 		    ZIO_PRIORITY_ASYNC_READ, ZIO_FLAG_CANFAIL,
 		    &aflags, zb) != 0) {
 			if (zfs_send_corrupt_data) {
-				uint64_t *ptr;
 				/* Send a block filled with 0x"zfs badd bloc" */
 				abuf = arc_buf_alloc(spa, blksz, &abuf,
 				    ARC_BUFC_DATA);
-				for (ptr = abuf->b_data;
-				    (char *)ptr < (char *)abuf->b_data + blksz;
-				    ptr++)
-					*ptr = 0x2f5baddb10cULL;
+
+				abd_iterate_wfunc(abuf->b_data, blksz, fillbadblock, NULL);
 			} else {
 				return (SET_ERROR(EIO));
 			}
 		}
 
+		buf = abd_borrow_linear_copy(abuf->b_data, blksz);
 		err = dump_data(dsp, type, zb->zb_object, zb->zb_blkid * blksz,
-		    blksz, bp, abuf->b_data);
+		    blksz, bp, buf);
+		abd_return_linear(abuf->b_data, buf, blksz);
 		(void) arc_buf_remove_ref(abuf, &abuf);
 	}
 
@@ -1231,11 +1241,11 @@ restore_object(struct restorearg *ra, objset_t *os, struct drr_object *drro)
 		dmu_buf_will_dirty(db, tx);
 
 		ASSERT3U(db->db_size, >=, drro->drr_bonuslen);
-		bcopy(data, db->db_data, drro->drr_bonuslen);
+		bcopy(data, ABD_TO_LINEAR(db->db_data), drro->drr_bonuslen);
 		if (ra->byteswap) {
 			dmu_object_byteswap_t byteswap =
 			    DMU_OT_BYTESWAP(drro->drr_bonustype);
-			dmu_ot_byteswap[byteswap].ob_func(db->db_data,
+			dmu_ot_byteswap[byteswap].ob_func(ABD_TO_LINEAR(db->db_data),
 			    drro->drr_bonuslen);
 		}
 		dmu_buf_rele(db, FTAG);
@@ -1326,6 +1336,7 @@ restore_write_byref(struct restorearg *ra, objset_t *os,
 	avl_index_t	where;
 	objset_t *ref_os = NULL;
 	dmu_buf_t *dbp;
+	void *buf;
 
 	if (drrwbr->drr_offset + drrwbr->drr_length < drrwbr->drr_offset)
 		return (SET_ERROR(EINVAL));
@@ -1360,8 +1371,10 @@ restore_write_byref(struct restorearg *ra, objset_t *os,
 		dmu_tx_abort(tx);
 		return (err);
 	}
+	buf = abd_borrow_linear_copy(dbp->db_data, drrwbr->drr_length);
 	dmu_write(os, drrwbr->drr_object,
-	    drrwbr->drr_offset, drrwbr->drr_length, dbp->db_data, tx);
+	    drrwbr->drr_offset, drrwbr->drr_length, buf, tx);
+	abd_return_linear(dbp->db_data, buf, drrwbr->drr_length);
 	dmu_buf_rele(dbp, FTAG);
 	dmu_tx_commit(tx);
 	return (0);
@@ -1408,7 +1421,7 @@ restore_spill(struct restorearg *ra, objset_t *os, struct drr_spill *drrs)
 	if (db_spill->db_size < drrs->drr_length)
 		VERIFY(0 == dbuf_spill_set_blksz(db_spill,
 		    drrs->drr_length, tx));
-	bcopy(data, db_spill->db_data, drrs->drr_length);
+	abd_copy_from_buf(db_spill->db_data, data, drrs->drr_length);
 
 	dmu_buf_rele(db, FTAG);
 	dmu_buf_rele(db_spill, FTAG);
